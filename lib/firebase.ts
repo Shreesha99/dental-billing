c98451844249384ad274af91ccd9fefc20dc0cfc
shreesha.venkatrm <shreesha.venkatram@siemensgamesa.com>
2025-10-24 17:03:18 +0530

    securing api keys

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -9,12 +9,12 @@ import {
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyATSHH1vuiInE10QiPDB2quVFGdo5dTopY",
-  authDomain: "dentist-billing.firebaseapp.com",
-  projectId: "dentist-billing",
-  storageBucket: "dentist-billing.appspot.com",
-  messagingSenderId: "36379471591",
-  appId: "1:36379471591:web:7e980826afd3562c287c42",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 export const firebaseApp = !getApps().length
